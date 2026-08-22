2f83a06c0e8f06e7aa69c294ab6ff2964940f727
RickyResQ <gottrauma@gmail.com>
2026-08-22 18:25:57 +0000

    chore: tutor page stub

---
+++ apps/web/app/tutor/page.tsx
@@ -0,0 +1,8 @@
+export default function TutorPage() {
+  return (
+    <main style={{ padding: 24 }}>
+      <h1>Tutor</h1>
+      <p>TODO Role 5/6: worksheet + evaluate flow</p>
+    </main>
+  );
+}
