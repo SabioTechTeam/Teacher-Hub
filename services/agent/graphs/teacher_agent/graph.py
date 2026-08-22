5d4a46e8f0bff87aab3fc8af35659506b858938d
RickyResQ <gottrauma@gmail.com>
2026-08-22 18:20:29 +0000

    chore: agent graph stub

---
+++ services/agent/graphs/teacher_agent/graph.py
@@ -0,0 +1,10 @@
+"""Sequential teacher agent graph stub.
+
+Order: diagnose -> select_strategy -> generate_worksheet -> evaluate -> update_student
+Replace with LangGraph when ready.
+"""
+from .state import AgentState
+
+def run(state: AgentState) -> AgentState:
+    # TODO: call node functions
+    return state
